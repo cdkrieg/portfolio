2020de0e249ec9ea60e3cb9df2a9b30eeff09432
cdkrieg <37251514+cdkrieg@users.noreply.github.com>
2022-03-28 21:33:40 -0500

    added bootstrap form controls to Contact

--- portfolio/src/Components/Contact/Contact.js
+++ portfolio/src/Components/Contact/Contact.js
@@ -1,44 +1,89 @@
-import React from 'react';
+import React from "react";
 
 const Contact = () => {
-    return ( 
-        <section className="project">
-                <div className="title">
-                    {/* <div className="col-xl-"> */}
-                        <h2 id="contact">Say Hello!</h2>
-                    {/* </div> */}
-                    
-                </div>
-                <br/><br/>
-                <form className="container" action="/form/submit" method="post">
-                    <div className="row">
-                        <div className="col-sm">
-                        <br/><br/>
-                            <label for="name">Name: </label>
-                            <input name="name" type="text" id="name" placeholder="Enter your name"/>                  
-                            <br/><br/>
-                            <label for="company">Company: </label>
-                            <input type="text" name="company" id="company" placeholder="Enter your company"/>
-                            <br/><br/>
-                            <label for="phone">Phone Number: </label>
-                            <input type="text" name="phone" id="phone" placeholder="Enter your phone #"/>
-                            <br/><br/>
-                            <label for="email">Email: </label>
-                            <input type="text" name="email" id="email" placeholder="Enter your email"/>
-                        </div>
-                        <div className="col-sm">
-                            <label for="message">*What would you like to say?</label>
-                            <br/>
-                            <textarea name="message" id="message" cols="30" rows="10" placeholder="Enter your message here"></textarea>
-                        </div>
-                    </div>
-                    <br/><br/>
-                    <div className="title">
-                        <input type="submit"/>
-                    </div>
-                </form>
-            </section>    
-     );
-}
- 
+  return (
+    <section className="project">
+      <div className="title">
+        {/* <div className="col-xl-"> */}
+        <h2 id="contact">Say Hello!</h2>
+        {/* </div> */}
+      </div>
+      <br />
+      <br />
+      <form className="form" role="form">
+        <div className="row">
+          <div className="col-sm">
+
+            <div className="form-group">
+              <label for="name">Name: </label>
+              <input
+                className="form-control"
+                name="name"
+                type="text"
+                id="name"
+                placeholder="Enter your name"
+              />
+            </div>
+            <br />
+            <br />
+            <div className="form-group">
+              <label for="company">Company: </label>
+              <input
+                className="form-control"
+                type="text"
+                name="company"
+                id="company"
+                placeholder="Enter your company"
+              />
+            </div>
+            <br />
+            <br />
+            <div className="form-group">
+              <label for="phone">Phone Number: </label>
+              <input
+                className="form-control"
+                type="text"
+                name="phone"
+                id="phone"
+                placeholder="Enter your phone #"
+              />
+            </div>
+            <br />
+            <br />
+            <div className="form-group">
+              <label for="email">Email: </label>
+              <input
+                className="form-control"
+                type="text"
+                name="email"
+                id="email"
+                placeholder="Enter your email"
+              />
+            </div>
+          </div>
+          <div className="col-sm">
+          <div className="form-group">
+            <label for="message">*What would you like to say?</label>
+            <br />
+            <textarea
+                className="form-control"
+              name="message"
+              id="message"
+              cols="30"
+              rows="10"
+              placeholder="Enter your message here"
+            ></textarea>
+            </div>
+          </div>
+        </div>
+        <br />
+        <br />
+        <div className="title">
+          <input type="submit"/>
+        </div>
+      </form>
+    </section>
+  );
+};
+
 export default Contact;
